99ee8f36a2de6d9204e2b2fd92cbb99ea996bdc8
Replit user <>
2023-08-19 21:24:29 +0000

    new try

--- exercises.c
+++ exercises.c
@@ -77,7 +77,7 @@ int *filterEvenNumbers(int arr[], int size, int *newSize) {
 
   for(int i = 0; i < size; i++)
   {
-    if(arr[i]%2 == 0
+    if(arr[i]%2 == 0)
     {
       nuevoArr[cantidadPares] = arr[i];
       cantidadPares++;
